6d2e6cc42703960eeaf6aa0c1f52652f7ff6092c
happy22867 <gairolasaumya27@gmail.com>
2026-02-19 20:36:03 +0530

    Updated API baseURL to deployed backend URL again

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://woodnest-1.onrender.com",
+  baseURL: "https://woodnest-1.onrender.com/api",
 });
 
 export default API;
